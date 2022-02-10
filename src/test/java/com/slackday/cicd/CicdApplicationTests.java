5c5f9435a595011cac4059f9c0054f347e7d423c
THIBAUT <thib.dhall@gmail.com>
2022-02-10 16:12:18 +0100

    Initial commit

---
+++ src/test/java/com/slackday/cicd/CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slackday.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
